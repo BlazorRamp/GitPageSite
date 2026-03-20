b7905c09c61dbeafed7453ad951d0e560ced12f2
Paul Kent <paulandrewkent@hotmail.com>
2026-03-20 19:52:32 +0000

    css fix

--- _content/BlazorRamp.Accordion/assets/js/accordion.js
+++ _content/BlazorRamp.Accordion/assets/js/accordion.js
@@ -1,23 +1,23 @@
 const _handleKeyDown = (e) => {
-    const keys = ['Home', 'End', 'ArrowUp', 'ArrowDown'];
+    const keys = ["Home", "End", "ArrowUp", "ArrowDown"];
     if (!keys.includes(e.key))
         return;
     const target = e.target;
-    if (target.closest('[data-br-accordion-trigger]'))
+    if (target.closest("[data-br-accordion-trigger]"))
         e.preventDefault();
 };
 const registerKeyHandler = (headerElementID) => {
     const headerElement = document.getElementById(headerElementID);
     if (!headerElement)
         return;
-    headerElement.removeEventListener('keydown', _handleKeyDown);
-    headerElement.addEventListener('keydown', _handleKeyDown);
+    headerElement.removeEventListener("keydown", _handleKeyDown);
+    headerElement.addEventListener("keydown", _handleKeyDown);
 };
 const unregisterKeyHandler = (headerElementID) => {
     const headerElement = document.getElementById(headerElementID);
     if (!headerElement)
         return;
-    headerElement.removeEventListener('keydown', _handleKeyDown);
+    headerElement.removeEventListener("keydown", _handleKeyDown);
 };
 export { registerKeyHandler, unregisterKeyHandler };
 //# sourceMappingURL=accordion.js.map
